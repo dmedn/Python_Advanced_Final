6524ff1a815eb303a3d42252daaa543c7398baca
mikhail <Darkmedan@yandex.ru>
2025-02-08 23:33:40 +0300

    Старт

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.12-slim
+WORKDIR /home
+COPY requirements.txt src/
+RUN pip install --upgrade pip
+RUN pip install -r /home/src/requirements.txt
+COPY src src/
+COPY static static/
+EXPOSE 8000
+CMD ["fastapi", "run", "src/main.py", "--host", "0.0.0.0", "--port", "8000"]
+
---
+++ src/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/api/users/me')
+async def get_users_me():
+    return {
+        'result': 'True',
+        'user': {
+            'id':1,
+            'name': 'Mikhail Arkhipychev',
+            'followers':[{'id':2, 'name':'Maria Arkhipycheva'}],
+            'following': [{'id':2, 'name': 'Maria Arkhipycheva'}],
+
+        },
+    }
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
