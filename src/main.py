cd61d8a3d19ae0096b090ac9feff7cadd09441fe
mikhail <Darkmedan@yandex.ru>
2025-02-09 14:56:12 +0300

    Redacting dockerfile

--- src/main.py
+++ src/main.py
@@ -16,5 +16,5 @@ async def get_users_me():
         },
     }
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+# if __name__ == '__main__':
+#     # uvicorn.run('main:app', host='0.0.0.0', port=8000)
